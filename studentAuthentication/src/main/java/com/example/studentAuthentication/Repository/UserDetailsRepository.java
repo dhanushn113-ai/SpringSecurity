d913024716bd94c993a1beb1997a0bb0a86efac0
dhanushn113-ai <dhanushn113@gmail.com>
2026-07-24 16:28:35 +0530

    m

--- studentAuthentication/src/main/java/com/example/studentAuthentication/Repository/UserDetailsRepository.java
+++
@@ -1,4 +0,0 @@
-package com.example.studentAuthentication.Repository;
-
-public interface UserDetailsRepository {
-}
--- studentAuthentication/src/main/java/com/example/studentAuthentication/model/Student.java
+++
@@ -1,4 +0,0 @@
-package com.example.studentAuthentication.model;
-
-public class Student {
-}
